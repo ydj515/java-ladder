6772027830f3f9c3e94962535795edf9fce0103b
ydj515 <ydj515@hanmail.net>
2019-11-07 11:44:21 +0900

    Main 작성

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import controller.LadderGameController;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		LadderGameController controller = new LadderGameController();
+		controller.run();
+	}
+
+}
